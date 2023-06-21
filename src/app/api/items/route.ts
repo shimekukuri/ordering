a0cc29104e8375c72038427e11d34323cd4180ad
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-06-21 11:57:07 -0400

    Added Get Route for Items

---
+++ src/app/api/items/route.ts
@@ -0,0 +1,8 @@
+import { prisma } from '@/ulitiles/prisma/db';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  const items = await prisma.item.findMany();
+
+  return NextResponse.json(items);
+}
