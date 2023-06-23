4ed35d6194c6bbcfa16e536f40559b3afdc80fce
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-06-23 10:36:43 -0400

    -

--- src/app/api/tests/userTests/route.tsx
+++
@@ -1,8 +0,0 @@
-import { prisma } from '@/ulitiles/prisma/db';
-import { NextResponse } from 'next/server';
-
-export const GET = async () => {
-  const users = await prisma.account.findMany();
-
-  return NextResponse.json(users);
-};
