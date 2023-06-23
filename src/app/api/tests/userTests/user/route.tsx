236b3004b855061f45c3e4505308641765834dea
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-06-23 13:01:43 -0400

    production push 4

--- src/app/api/tests/userTests/user/route.tsx
+++ src/app/api/tests/userTests/user/route.tsx
@@ -3,6 +3,6 @@ import { NextResponse } from 'next/server';
 
 export const GET = async () => {
   const users = await prisma.user.findMany();
-  //push
+  //push 1
   return NextResponse.json(users);
 };
