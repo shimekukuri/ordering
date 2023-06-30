40efe807d6547890c9c9b0edf1ab2607aa95bc9b
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-06-30 14:20:54 -0400

    Added Navigation Menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,28 @@ export default async function Home() {
         <Link href={'cartSearch/specialty'} className="btn join-item">
           Special
         </Link>
-        <button className="btn join-item">Button</button>
+        <Link href={'Search'} className="btn join-item bg-secondary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </Link>
+        <Link href={'cartSearch/snacks'} className="btn join-item">
+          Snacks
+        </Link>
+        <Link href={'cartSearch/other'} className="btn join-item">
+          Other
+        </Link>
       </div>
     </>
   );
