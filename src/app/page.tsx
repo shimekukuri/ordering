1936d6fad92481067e43ecb4a42fc82ddb665284
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-08-07 12:40:34 -0400

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import Links from "@/components/homePage/Links/Links";
-import Hero from "@/components/homePage/hero/hero";
-import NewsFeed from "@/components/homePage/newsFeed/NewsFeed";
+import Links from '@/components/homePage/Links/Links';
+import Hero from '@/components/homePage/hero/hero';
+import NewsFeed from '@/components/homePage/newsFeed/NewsFeed';
+import NewsFeed2 from '@/components/homePage/newsFeed/NewsFeed2';
 
 export default function Page() {
   return (
     <>
       <Hero />
-      <NewsFeed />
+      <NewsFeed2 />
       <Links />
     </>
   );
--- src/components/homePage/newsFeed/NewsFeed.tsx
+++ src/components/homePage/newsFeed/NewsFeed.tsx
@@ -3,92 +3,26 @@ import { useEffect, useRef, useState } from 'react';
 import NewsCard from '../newscard/NewsCard';
 
 export default function NewsFeed() {
-  let ref1 = useRef<HTMLDivElement>(null);
-  let ref2 = useRef<HTMLDivElement>(null);
-  let ref3 = useRef<HTMLDivElement>(null);
-  let ref4 = useRef<HTMLDivElement>(null);
-  let ref5 = useRef<HTMLDivElement>(null);
-  let ref6 = useRef<HTMLDivElement>(null);
-  let ref7 = useRef<HTMLDivElement>(null);
-  let ref8 = useRef<HTMLDivElement>(null);
-  let ref9 = useRef<HTMLDivElement>(null);
-  let ref10 = useRef<HTMLDivElement>(null);
-  let refArray = [ref1, ref2, ref3, ref4, ref5, ref6, ref7, ref8, ref9, ref10];
-
-  const disable = useState<boolean>(false);
-  const [loader, setLoader] = useState<boolean>(true);
-  const [isVisible, setIsVisible] = useState<boolean>(false);
-  const [inView, setInView] = useState<boolean>(false);
-  const observed = useRef(null);
-  const [once, setOnce] = useState<boolean>(false);
+  const [loader, setLoader] = useState<boolean>(false);
 
   useEffect(() => {
-    const reff = observed;
-    const observer = new IntersectionObserver(
-      (e) => {
-        if (observed.current) {
-          setInView(() => e[0].isIntersecting);
-          if (e[0].isIntersecting) {
-            setOnce(() => true);
-          }
-        }
-      },
-      { threshold: 0.9 }
-    );
-
-    if (reff.current) {
-      observer.observe(reff.current);
-    }
+    const timer = setTimeout(() => {
+      setLoader(true);
+    }, 1000);
 
     return () => {
-      if (reff.current) {
-        observer.unobserve(reff.current);
-      }
+      clearTimeout(timer);
     };
   }, []);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoader(false);
-    }, 1000);
-  });
-
-  useEffect(() => {
-    let timers = refArray.map((x, i) => {
-      if (!x?.current) {
-        return 999;
-      }
-      if (!inView) {
-        return;
-      }
-
-      let timer = setTimeout(() => {
-        x.current?.scrollIntoView({
-          behavior: 'smooth',
-        });
-      }, (i + 1) * 3000);
-
-      return timer;
-    });
-
-    return () => {
-      timers.forEach((x) => {
-        clearTimeout(x);
-      });
-    };
-  }, [loader, inView]);
-
   return (
     <>
-      <div
-        className="relative flex flex-1 min-h-[90dvh] max-w-[100vw]"
-        ref={observed}
-      >
+      <div className="relative flex flex-1 max-w-[100vw]">
         {loader ? (
           <div className="loading loading-md loading-ring justify-self-center self-center flex-1"></div>
         ) : (
           <div className="flex flex-1 flex-row overflow-x-scroll snap-x">
-            {refArray.map((x, i) => {
+            {[...Array(10)].map((x, i) => {
               return <NewsCard id={`newscard${i}`} key={i} reference={x} />;
             })}
             <div className="absolute left-0 bottom-0 top-0 flex items-center">
