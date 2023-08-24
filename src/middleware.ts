0561047f105a38f8c24e634646d05b08b4345aed
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-08-24 12:44:05 -0400

    Fixed?

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,8 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { Session } from '@prisma/client';
-import { PrismaClient } from '@prisma/client/edge';
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  let prisma = new PrismaClient();
-  let session = request.cookies.get(
-    '__Secure-next-auth.session-token'
-  ) as unknown as Session | undefined;
-
-  if (!session) {
-    return;
-  }
-
-  let user = await prisma.session.findFirst({
-    where: { sessionToken: session.sessionToken },
-    include: { user: { include: { permissions: {} } } },
-  });
-  console.log(user);
-
   return NextResponse.next();
 }
 
