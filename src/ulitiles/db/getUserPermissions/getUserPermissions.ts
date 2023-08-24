37098f078e01ecfa4f6857b1ff577cb27003ce46
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-08-24 14:17:24 -0400

    -

--- src/ulitiles/db/getUserPermissions/getUserPermissions.ts
+++ src/ulitiles/db/getUserPermissions/getUserPermissions.ts
@@ -3,10 +3,13 @@ import { prisma } from '@/ulitiles/prisma/db';
 import { getServerSession } from 'next-auth';
 
 export const getUserPermissions = async () => {
+  console.log('holo');
   const session = await getServerSession(OPTIONS);
+  console.log('session', session);
   if (!session?.user?.email) {
     return new Error('Session Not Found');
   }
+  console.log('meep');
   const userPermissions = await prisma.user.findFirst({
     where: { email: session.user.email },
     include: { permissions: {} },
