f5a0b8ae1511aee6a44c94be3cd4a5b8b3643be1
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-08-25 16:19:12 -0400

    ----

--- src/app/accessnet/admin/user-maintenance/delete-user/page.tsx
+++ src/app/accessnet/admin/user-maintenance/delete-user/page.tsx
@@ -13,9 +13,57 @@ export default async function page() {
     return redirect('/unauthorized');
   }
 
-  return (
-    <div className="w-screen min-h-screen max-w-[100vw] flex justify-center items-center">
-      Delete User
+  const deleteUser = async (data: FormData) => {
+    'use server';
+    
+
+  
+    try {
+      console.log('meep')
+    } catch (error) {
+      console.log(error);
+      redirect('./failure');
+    }
+    return redirect('./success');
+  };
+
+    return (
+    <div className="h-full flex-1 flex justify-center items-center p-4">
+      <form
+        className="h-full w-full lg:w-2/3 lg:h-2/3 flex flex-col justify-center items-center gap-4"
+        action={deleteUser}
+      >
+        <h1 className="text-3xl text-center">Submit New Maintenance Ticket</h1>
+        <div className="divider"></div>
+        <input
+          required
+          type="text"
+          placeholder="Subject"
+          name="subject"
+          className="input input-bordered input-primary w-full max-w-xs shadow-xl"
+        />
+        <select
+          className="select select-secondary w-full max-w-xs shadow-xl"
+          name="location"
+        >
+          <option>Please Select a location</option>
+          <option>access</option>
+          <option>whitwell</option>
+          <option>jasper</option>
+          <option>cates</option>
+          <option value={'uniformshop'}> Uniform Shop</option>
+        </select>
+        <textarea
+          required
+          placeholder="Description"
+          name="description"
+          className="textarea textarea-bordered textarea-accent w-full max-w-xs shadow-xl"
+        />
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
+      </form>
     </div>
+  );
   );
 }
