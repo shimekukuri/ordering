7dc00a003514cfc08ddd2532ec1e91c22702d43e
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-08-30 15:27:22 -0400

    test prod

--- src/app/api/users/find-user/route.tsx
+++ src/app/api/users/find-user/route.tsx
@@ -17,20 +17,21 @@ export async function POST(request: NextRequest, response: NextResponse) {
   ]);
   if (!permCheck) {
     NextResponse.json({ message: 'NAHAHAHAHA_NOPE' });
+    return;
+  } else {
+    const body = await request.json();
+    const { user } = body;
+
+    if (user === undefined) {
+      return NextResponse.json({ message: 'Bad Request', status: 400 });
+    }
+
+    let found = await prisma.user.findFirst({
+      where: { email: user },
+    });
+
+    return NextResponse.json({ found });
   }
-
-  const body = await request.json();
-  const { user } = body;
-
-  if (user === undefined) {
-    return NextResponse.json({ message: 'Bad Request', status: 400 });
-  }
-
-  let found = await prisma.user.findFirst({
-    where: { email: user },
-  });
-
-  return NextResponse.json({ found });
 }
 
 export const dynamic = 'force-dynamic';
