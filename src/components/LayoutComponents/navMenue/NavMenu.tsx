37cd08cef285948470f2f8417ef15159d5b361b5
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-09-11 11:02:33 -0400

    test prod

--- src/components/LayoutComponents/navMenue/NavMenu.tsx
+++ src/components/LayoutComponents/navMenue/NavMenu.tsx
@@ -30,21 +30,37 @@ export default async function NavMenu({
         </label>
         <Link
           href={'/accessnet'}
-          className="w-full min-h-[25vh] bg-red-500 border-t-2 border-b-2 border-black opacity-0 group-[.inview]:animate-slidein-400"
+          className="w-full min-h-[25vh] bg-red-500 border-t-2 border-b-2 border-black opacity-0 group-[.inview]:animate-slidein-400 flex flex-wrap items-center md:flex-nowrap justify-around"
+          onClick={() => closeDrawer()}
+        >
+          <div className="flex-1 flex justify-center items-center">
+            Access Net
+          </div>
+          <div className="grid grid-cols-2 grid-rows-2 flex-1 justify-items-center items-center md:flex gap-4 h-full p-2">
+            <span className="rounded-full bg-blue-500 aspect-square h-full md:h-auto md:w-[25%] flex justify-center items-center ">
+              test
+            </span>
+            <span className="rounded-full bg-blue-500 aspect-square h-full md:h-auto md:w-[25%] flex justify-center items-center ">
+              test
+            </span>
+            <span className="rounded-full bg-blue-500 aspect-square h-full md:h-auto md:w-[25%] flex justify-center items-center ">
+              test
+            </span>
+            <span className="rounded-full bg-blue-500 aspect-square h-full md:h-auto md:w-[25%] flex justify-center items-center ">
+              test
+            </span>
+          </div>
+        </Link>
+        <Link
+          href={'/accessnet'}
+          className="w-full min-h-[25vh] bg-red-500 border-t-2 border-b-2 border-black opacity-0 group-[.inview]:animate-slidein-600 flex flex-wrap items-center md:flex-nowrap "
           onClick={() => closeDrawer()}
         >
           <div>Access Net</div>
         </Link>
         <Link
           href={'/accessnet'}
-          className="w-full min-h-[25vh] bg-red-500 border-t-2 border-b-2 border-black opacity-0 group-[.inview]:animate-slidein-600"
-          onClick={() => closeDrawer()}
-        >
-          <div>Access Net</div>
-        </Link>
-        <Link
-          href={'/accessnet'}
-          className="w-full min-h-[25vh] bg-red-500 border-t-2 border-b-2 border-black opacity-0 group-[.inview]:animate-slidein-800"
+          className="w-full min-h-[25vh] bg-red-500 border-t-2 border-b-2 border-black opacity-0 group-[.inview]:animate-slidein-800 flex flex-wrap items-center md:flex-nowrap"
           onClick={() => closeDrawer()}
         >
           <div>Access Net</div>
@@ -52,7 +68,7 @@ export default async function NavMenu({
         <Link
           href={'/accessnet'}
           onClick={() => closeDrawer()}
-          className="w-full min-h-[25vh] bg-red-500 border-t-2 border-b-2 border-black opacity-0 group-[.inview]:animate-slidein-1000"
+          className="w-full min-h-[25vh] bg-red-500 border-t-2 border-b-2 border-black opacity-0 group-[.inview]:animate-slidein-1000 flex flex-wrap items-center md:flex-nowrap"
         >
           <div>Access Net</div>
         </Link>
