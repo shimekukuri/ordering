5a4140c052f316ceb21acaf7af0309faa069b50c
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-09-11 16:25:05 -0400

    Test Prod

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex-1 max-h-screen flex items-center justify-center">
+    <div className="flex items-center justify-center absolute right-0 left-0 top-0 bottom-0 hero-overlay">
       <div className="w-1/2 flex">
         <div className="flex-1 loading loading-infinity"></div>
       </div>
