85ceab373da494ae6cea48577455a953bfef1698
Rachel Dooley <102691550+Rae1821@users.noreply.github.com>
2023-10-04 10:05:47 -0400

    Added css modules and jsx for heroicons

--- src/components/access-specialized-care/features/Features.tsx
+++ src/components/access-specialized-care/features/Features.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import { CheckCircleIcon } from '@heroicons/react/24/solid';
 
 const Features = () => {
   return (
@@ -14,13 +13,17 @@ const Features = () => {
             <ul className="mt-4 flex flex-col">
               <li className="flex my-4 text-sm justify-start text-gray-700">
                 <span className="text-green-800 mr-2">
-                    <CheckCircleIcon className="w-5 h-5" />
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                  <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+                  </svg>
                 </span>
                 3 different packing systems to choose from
               </li>
               <li className="flex my-4 text-sm justify-start text-gray-700">
                 <span className="text-green-800 mr-2">
-                  <CheckCircleIcon className="w-5 h-5" />
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                  <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+                  </svg>
                 </span>
                 Extensive drug utilization review
               </li>
@@ -33,19 +36,25 @@ const Features = () => {
             <ul className="mt-4 flex flex-col">
               <li className="text-sm flex my-4 leading-relaxed justify-start text-slate-700">
                 <span className="text-green-800 mr-2">
-                  <CheckCircleIcon className="w-5 h-5" />
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                  <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+                  </svg>
                 </span>
                   State of the art medication packing system
               </li>
               <li className="text-sm flex my-4 justify-start text-gray-700">
                 <span className="text-green-800 mr-2">
-                  <CheckCircleIcon className="w-5 h-5" />
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                    <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+                    </svg>
                 </span>
                   Convenient to manage and dispense accurately
               </li>
               <li className="text-sm flex my-4 justify-start text-gray-700">
                 <span className="text-green-800 mr-2">
-                  <CheckCircleIcon className="w-5 h-5" />
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                  <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+                  </svg>
                 </span>
                   Medication carts, E-Mar system, drug disposal, and more!
               </li>
@@ -58,13 +67,17 @@ const Features = () => {
             <ul className="mt-4 flex flex-col">
               <li className="text-sm flex my-4 justify-start text-gray-700">
                 <span className="text-green-800 mr-2">
-                  <CheckCircleIcon className="w-5 h-5" />
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                  <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+                  </svg>
                 </span>
                   Ability to make immediate prescription changes + same day delivery
               </li>
               <li className="text-sm flex my-4 justify-start text-gray-700">
                 <span className="text-green-800 mr-2">
-                  <CheckCircleIcon className="w-5 h-5" />
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                  <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+                  </svg>
                 </span>
                   Acceptance of all major insurance plans
               </li>
@@ -77,25 +90,33 @@ const Features = () => {
             <ul className="mt-4 flex flex-col">
               <li className="text-sm flex my-4 justify-start text-slate-800">
                 <span className="text-green-800 mr-2">
-                  <CheckCircleIcon className="w-5 h-5" />
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                  <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+                  </svg>
                 </span>
                 Fast, local delivery. Licensed in TN & GA
               </li>
               <li className="text-sm flex mt-4 mb-4 justify-start text-slate-800">
                 <span className="text-green-800 mr-2">
-                  <CheckCircleIcon className="w-5 h-5" />
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                  <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+                  </svg>
                 </span>
                 On site diabetic fittings and screenings
               </li>
               <li className="text-sm flex my-4 justify-start text-slate-800">
                 <span className="text-green-800 mr-2">
-                  <CheckCircleIcon className="w-5 h-5" />
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                  <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+                  </svg>
                 </span>
                 On site vaccinations
               </li>
               <li className="text-sm flex my-4 justify-start text-slate-800">
                 <span className="text-green-800 mr-2">
-                  <CheckCircleIcon className="w-5 h-5" />
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                  <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+                  </svg>
                 </span>
                 6 pharmacists on call
               </li>
@@ -115,4 +136,4 @@ const Features = () => {
   )
 }
 
-export default Features
+export default Features;
