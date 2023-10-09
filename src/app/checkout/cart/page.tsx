35bd0afa06120dc56bd8dde7317f919383e5aff9
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-10-09 13:13:44 -0400

    fffff

--- src/app/checkout/cart/page.tsx
+++ src/app/checkout/cart/page.tsx
@@ -28,7 +28,7 @@ export default async function Page() {
 
   if (!order?.[0]) {
     return (
-      <div className="p-4 flex-1 flex items-center justify-center text-center">
+      <div className="p-4 flex-1 flex items-center justify-center text-center flex-col">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -43,7 +43,6 @@ export default async function Page() {
             d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
           />
         </svg>
-
         <div className="flex-1 text-7xl">Cart Empty</div>
       </div>
     );
