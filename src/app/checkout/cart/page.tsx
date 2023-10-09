baf3d022f00f1b752e537131e82eaf709fa1958e
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-10-09 13:16:08 -0400

    fff

--- src/app/checkout/cart/page.tsx
+++ src/app/checkout/cart/page.tsx
@@ -35,7 +35,7 @@ export default async function Page() {
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-6 h-6"
+          className="w-16 h-16"
         >
           <path
             strokeLinecap="round"
@@ -43,7 +43,7 @@ export default async function Page() {
             d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
           />
         </svg>
-        <div className="flex-1 text-7xl">Cart Empty</div>
+        <div className="text-7xl">Cart Empty</div>
       </div>
     );
   }
