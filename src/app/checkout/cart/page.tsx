06a5d3eb831d4413453b3db8755064e792e05845
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-10-09 11:19:54 -0400

    Tes Prod

--- src/app/checkout/cart/page.tsx
+++ src/app/checkout/cart/page.tsx
@@ -6,7 +6,11 @@ export default async function Page() {
   const session = await getServerSession(OPTIONS);
   const cart = await prisma.user.findFirst({
     where: { email: session?.user?.email },
-    include: { accounts: { include: { Order: { include: { items: {} } } } } },
+    include: {
+      accounts: {
+        include: { Order: { include: { items: { include: { item: {} } } } } },
+      },
+    },
   });
 
   const order = cart?.accounts[0].Order[0].items;
@@ -15,7 +19,12 @@ export default async function Page() {
   return (
     <div>
       {order?.map((x, i) => {
-        return <div key={x.id}>{x.id}</div>;
+        return (
+          <div key={x.id}>
+            <div>{x.item.id}</div>
+            <div>{x.item.name}</div>
+          </div>
+        );
       })}
     </div>
   );
